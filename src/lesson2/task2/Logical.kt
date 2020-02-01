7fd4cd5c888494560d30ada6b0f1ad856172bf19
OneLander <flameplay.inc@gmail.com>
2020-02-01 19:08:41 +0300

    5

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -43,7 +43,11 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int = when (month) {
+    2 ->  if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))) 29 else 28
+    1, 3, 5, 7, 8, 10, 12 -> 31
+    else -> 30
+}
 
 /**
  * Средняя
